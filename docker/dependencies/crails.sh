408f4758dafca38d202d8390e934c8f67e288a54
Michael <michael@unetresgrossebite.com>
2018-06-12 22:26:09 +0200

    build crails with all options off by default

--- docker/dependencies/crails.sh
+++ docker/dependencies/crails.sh
@@ -7,12 +7,11 @@ git checkout 3ac893fb8a988dad614d715aefac5808f8af0518
 mkdir build
 cd build
 
-sed -e '47d' ../CMakeLists.txt > CMakeLists.txt
-mv CMakeLists.txt ..
-
 cmake -DDEVELOPER_MODE=OFF \
       -DUSE_MEMCACHED=OFF \
       -DUSE_MONGODB=OFF \
+      -DUSE_ODB=OFF \
       -DUSE_COOKIE_CIPHER=OFF \
+      -DUSE_SSL=OFF \
       ..
 make && make install

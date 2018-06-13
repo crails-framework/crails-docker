fb9b876270aa96b74eae83f8b6176b0b4b2980fc
Michael <michael@unetresgrossebite.com>
2018-06-13 13:44:15 +0200

    update heroku-buildpack-crails url in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ docker/shell -v -c package.sh
 heroku config $* | grep LD_LIBRARY_PATH
 if [[ $? != 0 ]] ; then
   heroku config:set LD_LIBRARY_PATH=/app/lib:/app/bin $*
-  heroku config:set BUILDPACK_URL=https://github.com/Plaristote/heroku-buildpack-crails $*
+  heroku config:set BUILDPACK_URL=https://github.com/crails-framework/heroku-buildpack-crails.git $*
 fi
 
 cd runtime
